d1eae94bd0f91a18989d846eb5a2e2efdf57227c
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-08-24 13:05:03 +0200

    Fix transaction view model: use users ID's instead of their names

--- ViewModels/Transaction/GetTransactionsViewModel.cs
+++ ViewModels/Transaction/GetTransactionsViewModel.cs
@@ -2,7 +2,7 @@
 namespace ImprovedPicpay.ViewModels.Transaction;
 
 public record struct GetTransactionsViewModel(string id,
-                                              string senderName,
-                                              string receiverName,
+                                              string senderId,
+                                              string receiverId,
                                               decimal amount,
                                               DateTime timestamp);
