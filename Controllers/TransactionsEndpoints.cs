b5631303ee0b6e63d8798deeac46d14bbedb8ed2
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-10-27 16:31:37 +0200

    Refactor: convert transaction controller to mininal API endpoint

---
+++ Controllers/TransactionsEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using ImprovedPicpay.Services;
+using ImprovedPicpay.ViewModels.Transaction;
+
+namespace ImprovedPicpay.Controllers;
+
+public class TransactionsEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var route = app.MapGroup("api/transaction");
+
+        route.MapGet("", async (TransactionService service) =>
+        {
+            return Results.Ok(await service.GetAllAsync());
+        });
+
+        route.MapGet("{senderId}", async (string senderId, TransactionService service) =>
+        {
+            return Results.Ok(await service.GetByAsync(senderId));
+        });
+
+        route.MapPost("", async (AddTransactionViewModel model, TransactionService service) =>
+        {
+            var response = await service.CreateAsync(model);
+
+            return Results.Ok(response);
+        });
+    }
+}
